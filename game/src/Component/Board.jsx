62425ea17766e8969d294b05be69967038174295
Vinderruck <majevincent0@gmail.com>
2024-09-01 18:06:51 +0300

    Finilising

--- game/src/Component/Board.jsx
+++ game/src/Component/Board.jsx
@@ -1,35 +1,51 @@
 import React,{useState,useEffect} from 'react'
 import {motion} from 'framer-motion';
-
+ 
+import blank from  './assets/blank.jpeg'
+ import red from './assets/red.jpeg';
+ import blue from './assets/blue.jpeg';
+ import Yellow from './assets/Yellow.jpeg';
+ import pink from './assets/pink.jpeg';
+ import green from './assets/green.jpeg';
+ import Brown from './assets/Brown.jpeg';
+ 
+ 
 
 
 const Colors=[
-    'Red',
-    'Orange',
-    'Skyblue',
-    'Yellow',
-    'Indigo',
-    'Green' 
+    red,
+    blue,
+    Yellow,
+    pink,
+    green,
+    Brown
 ]
  const width=8;
 const Board = () => {
     const [CurrentColor, setCurrentColor] = useState([])
+  const [SquareBeignDragged, setSquareBeignDragged] = useState(null);
+  const [SquareBeignReplaced, setSquareBeignReplaced] = useState(null)
+
+
+
     const Chexkthree = ()=>{
-        for(let i= 0;  i < 47; i++){
+        for(let i= 0;  i <= 47; i++){
             const threesqure = [ i, i + width, i + width * 2];
             const DescidedColor = CurrentColor[i]
             if(threesqure.every(Squar => CurrentColor[Squar] === DescidedColor)){
-                threesqure.forEach(Squar => CurrentColor[Squar]='')
+                threesqure.forEach(Squar => CurrentColor[Squar]=blank)
+                return true
             }
         }
     }
 
     const Chexkfour = ()=>{
-        for(let i= 0;  i < 39; i++){
+        for(let i= 0;  i <=39; i++){
             const foursqure = [ i, i + width, i + width * 2, i + width * 3];
             const DescidedColor = CurrentColor[i]
             if(foursqure.every(Squar => CurrentColor[Squar] === DescidedColor)){
-                foursqure.forEach(Squar => CurrentColor[Squar]='')
+                foursqure.forEach(Squar => CurrentColor[Squar]= blank)
+                return true
             }
         }
     }
@@ -41,7 +57,8 @@ const Chexkrowthree = ()=>{
         const invalid=[6,7,14,15,22,23,30,31,38,39,46,47,54,55,63,64]
         if(invalid.includes(i))continue
         if( rowthreesqure.every(Squar => CurrentColor[Squar] === DescidedColor)){
-            rowthreesqure.forEach(Squar => CurrentColor[Squar]='')
+            rowthreesqure.forEach(Squar => CurrentColor[Squar]=  blank)
+            return true
         }
     }
 }
@@ -52,7 +69,8 @@ const Chexkrowfour = ()=>{
         const invalid=[5,6,7,13 ,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55,62,63,64];
         if(invalid.includes(i))continue
         if(fourrowsqure.every(Squar => CurrentColor[Squar] === DescidedColor)){
-            fourrowsqure.forEach(Squar => CurrentColor[Squar]='')
+            fourrowsqure.forEach(Squar => CurrentColor[Squar]=   blank)
+            return true
         }
     }
 }
@@ -63,28 +81,66 @@ const Chexkrowfour = ()=>{
 
 
 const Movedown = () =>{
-    for(let i = 0; i < 64 - width ; i++){
+    for(let i = 0; i <= 55; i++){
 
          const firstRow = [0,1,2,3,4,5,6,7];
          const isFirst = firstRow.includes(i);
 
 
-         if(isFirst && CurrentColor[i]=== ''){
+         if(isFirst && CurrentColor[i]===   blank){
             let RandomNumber = Math.floor(Math.random() * Colors.length);
             CurrentColor[i] = Colors[RandomNumber]
         }
-        if(CurrentColor[i + width]===''){
+        if(CurrentColor[i + width]===   blank){
             CurrentColor[i + width]=CurrentColor[i];
-            CurrentColor[i]='';
+            CurrentColor[i]=   blank;
         }
        
     }
 }
 
+const DragStart = (e) =>{
+
+setSquareBeignDragged(e.target)
+}
+const DragEnd = (e) =>{
+ const SqureDraggedId = parseInt(SquareBeignDragged.getAttribute('data-id'));
+ const SquareReplacedId = parseInt(SquareBeignReplaced.getAttribute('data-id'))
 
 
+ //switching the background
+CurrentColor[SqureDraggedId] = SquareBeignReplaced.getAttribute('src')
+CurrentColor[SquareReplacedId]=SquareBeignDragged.getAttribute('src')
+ console.log(SqureDraggedId);
+ console.log("SqureReplaced",SquareReplacedId)
 
 
+//cMaking valid move
+const Validmoves =[
+    SqureDraggedId - 1,
+    SqureDraggedId - width,
+    SqureDraggedId + 1,
+    SqureDraggedId + width
+]
+ //defining a valid move
+ const Validmove = Validmoves.includes(SquareReplacedId)
+ const isroethree =Chexkrowthree();
+ const isrowfour =Chexkrowfour()
+ const iscolumnthree =Chexkthree();
+ const uscolumnfour =  Chexkfour ()
+
+
+ if(SquareReplacedId 
+    && Validmove 
+    && (isroethree|| isrowfour||uscolumnfour ||iscolumnthree)){
+   setSquareBeignDragged(null)
+   setSquareBeignReplaced(null)
+ } else{
+    CurrentColor[SqureDraggedId]=SquareBeignDragged.getAttribute('src')
+   CurrentColor[SquareReplacedId]=SquareBeignReplaced.getAttribute('src')
+   setCurrentColor([...CurrentColor])
+ }
+}
 
 
 //creating board
@@ -103,7 +159,7 @@ const Movedown = () =>{
         CreateBoard()
       
     }, [])
-    console.log(CurrentColor)
+
     useEffect(()=>{
         const time = setInterval(()=>{
             Chexkfour ()
@@ -115,23 +171,30 @@ const Movedown = () =>{
         },100)
         return()=> clearInterval(time)
     },[Chexkthree, Chexkrowthree,Chexkrowfour, Chexkfour,Movedown ,CurrentColor ])
+//Function that will run drop
+const Drop =(e)=>{
+console.log(e.target)
+setSquareBeignReplaced(e.target)
+}
 
-   
+
+
   return (
-    <motion.div claassName="board">
+    <motion.div className="board">
         <div className="boardgame">
             {CurrentColor.map((color,index)=>(
-  <img alt='image' style={{backgroundColor:color}}
-  
+  <img  src={{color}} alt={color}
+  key={index}
   
   data-id={index}
   draggable={true}
-  onDragEnd={{}}
-  onDragStart={{}}
+ 
+  onDragStart={DragStart}
   onDragEnter={(e)=>e.preventDefault()}
   onDragLeave={(e)=>e.preventDefault()}
-  omDragOver={(e)=>e.preventDefault()}
-  onDrop={{}}
+  onDragOver={(e)=>e.preventDefault()}
+  onDrop={Drop}
+  onDragEnd={DragEnd}
   />
             ))}
             
